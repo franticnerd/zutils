09f8f0c999882c84e394819e7206c74eefbebbe3
franticnerd <chao.uiuc@gmail.com>
2017-04-15 23:40:49 -0400

    clean

--- datasets/cleaning/scripts/checkins-ny.sh
+++ datasets/cleaning/scripts/checkins-ny.sh
@@ -3,8 +3,14 @@ para_file='./checkins-ny.yaml'
 # clean tweets
 python '../extract_clean_tweets.py' $para_file
 
-# # extract messages
-# python '../dump_clean_tweet_messages.py' $para_file
+# extract messages
+python '../dump_clean_tweet_messages.py' $para_file
 
-# # extract pos tags
-# python '../pos-tagging.py' $para_file
+# extract pos tags
+python '../pos-tagging.py' $para_file
+
+# extract trajectories
+python '../extract_trajectories.py' $para_file
+
+# extract dense trajectories
+python '../extract_dense_trajectories.py' $para_file
