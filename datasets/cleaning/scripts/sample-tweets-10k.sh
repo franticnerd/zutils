6645d1657c3ef94dbfbbf4b450182702bef5e538
chao <chao.uiuc@gmail.com>
2017-07-03 02:38:52 -0500

    scripts updated

--- datasets/cleaning/scripts/sample-tweets-10k.sh
+++ datasets/cleaning/scripts/sample-tweets-10k.sh
@@ -4,19 +4,19 @@ para_file='./sample-tweets-10k.yaml'
 python '../extract_clean_tweets.py' $para_file
 
 # downsample tweets
-python '../downsample_clean_tweets.py' $para_file
+# python '../downsample_clean_tweets.py' $para_file
 
 # extract messages
-python '../dump_clean_tweet_messages.py' $para_file
+# python '../dump_clean_tweet_messages.py' $para_file
 
 # extract pos tags
 python '../pos-tagging.py' $para_file
 
 # extract activity tweets
-python '../extract_activity_tweets.py' $para_file
+# python '../extract_activity_tweets.py' $para_file
 
 # extract trajectories
-python '../extract_trajectories.py' $para_file
+# python '../extract_trajectories.py' $para_file
 
 # extract dense trajectories
-python '../extract_dense_trajectories.py' $para_file
+# python '../extract_dense_trajectories.py' $para_file
